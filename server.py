63f75facf7a271a141cd6961599e3c6fa4bacb5e
Nanda Kishore B <pgsbssnk@gmail.com>
2022-02-06 20:35:56 +0530

    send whatsapp message when someone uses Productivity Manager desktop ui to access server.py

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import socket
 import pickle
+import pyautogui as pt
+from time import sleep
+from message_process import *
+
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 47))
 print(socket.gethostname())
@@ -25,6 +29,38 @@ while True:
         # continue to listen for connections
         continue
 
+    # identify whatsapp icon, even if it has new msgs or not
+    position = pt.locateOnScreen("wt_new_msg.png", confidence=.9)
+    if(position == None):
+        position = pt.locateOnScreen("wt_no_new_msg.png", confidence=.9)
+
+    # if unable to identify wt icon, stop program
+    if(position == None):
+        print('Some trouble in finding wt icon, stoping server')
+        exit()
+
+    # open whatsapp by clicking on whatsapp icon
+    x = position[0]
+    y = position[1]
+    pt.moveTo(x+17, y+17, duration=.05)
+    pt.click()
+
+    # by default it will be inside productivity manager group
+    # locate smiley and click on typing bar
+    position = pt.locateOnScreen("smiley_paperclip.png", confidence=.6)
+    x = position[0]
+    y = position[1]
+    pt.moveTo(x+150, y+15, duration=.005)
+    pt.click()
+
+    msg = 'Someone is now using the desktop UI of productivity manager. '
+    msg += 'Whatsapp service will resume shortly after that user quits.'
+    # type msg and send
+    pt.typewrite(msg,interval=0.001)
+    pt.typewrite("\n",interval=0.001)
+
+    user_using = 'unknown'
+
     while(True):
         encoded = clt.recv(1024)
         string_command = encoded.decode("utf-8")
@@ -36,6 +72,11 @@ while True:
 
         else:
             try:
+                # code to identify user using server
+                search_query = 'SELECT * FROM USER WHERE user_name ='
+                if(string_command.startswith(search_query)):
+                    user_using = string_command[len(search_query):]
+
                 cur.execute(string_command)
                 conn.commit()
                 result = cur.fetchall()
@@ -63,4 +104,20 @@ while True:
             clt.send(data)
 
     clt.close()
+
+    msg = 'The one who used the productivity manager desktop app is ' + user_using
+
+    # type msg and send
+    pt.typewrite(msg,interval=0.001)
+    pt.typewrite("\n",interval=0.001)
+
+    # minimize whatsapp as there are no new messages
+    position = pt.locateOnScreen("wt_bar.png", confidence=.9)
+    x = position[0]
+    y = position[1]
+
+    # move mouse pointer over minimize icon and click it
+    pt.moveTo(x+917, y+17, duration=.05)
+    pt.click()
+    
     conn.close()
